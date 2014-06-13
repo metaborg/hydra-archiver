9ade803f0f17e5d82b694078d541492a012254ba
Vlad Vergu <v.vergu@gmail.com>
2014-06-13 13:16:02 +0200

    Organize imports

--- src/org/metaborg/spoofax/updatesite/archiver/Archiver.java
+++ src/org/metaborg/spoofax/updatesite/archiver/Archiver.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.SimpleDateFormat;
